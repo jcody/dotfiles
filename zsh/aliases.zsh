5d329af6116f957290589a1b8a8dda9943a83536
Joey Cody <joeydcody@gmail.com>
2014-04-28 23:19:20 -0700

    using zsh, removed shell scripts.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,32 @@
 alias reload!='. ~/.zshrc'
+
+### Directories
+alias ht="cd ~/Projects/ht"
+alias cfa="cd ~/Projects/codeforamerica"
+alias exercism="/Users/jcody/Projects/exercism/exercism"
+
+### Terminal
+alias vi="vim"
+alias s.="subl ."
+alias clr="clear"
+alias ls="gls -F --color"
+alias l="gls -lAh --color"
+alias ll="gls -l --color"
+alias la='gls -A --color'
+
+
+### Git
+alias gb='git branch'
+alias gs='git status -sb'
+alias gaa='git add -A'
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gco='git checkout'
+
+### Ruby
+alias rc='rails console'
+alias migrate="bundle exec rake db:migrate db:test:prepare"
+alias seed="bundle exec rake db:seed"
+alias remigrate="bundle exec rake db:migrate db:migrate:redo db:schema:dump db:test:prepare"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,4 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 if (( $+commands[git] ))
@@ -21,7 +20,7 @@ git_dirty() {
   else
     if [[ "$st" =~ ^nothing ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "on %{$fg_bold[yellow]%}$(git_prompt_info)%{$reset_color%}"
     else
       echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     fi
@@ -34,48 +33,20 @@ git_prompt_info () {
  echo "${ref#refs/heads/}"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
-}
-
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[green]%}$PWD%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+user_name() {
+  echo "%{$fg_bold[red]%}$USER%\:%{$reset_color%}"
 }
+
+export PROMPT=$'\n$(user_name) $(directory_name) $(git_dirty)\n› '
+
+set_prompt () {
+  export RPROMPT="%{$fg_bold[red]%}%{$reset_color%}"
+}
+
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
