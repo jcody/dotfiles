7c16f122401af03773a41bc3013e8bb75202521a
Joey Cody <joeydcody@gmail.com>
2016-03-28 10:00:36 -0700

    gopath update

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="$HOME/code/go/bin:$ZSH/bin:$PATH"
+
